ee8112987ad589ef86b5cf13fd5c3717a28f6bf6
Jazmin Matos <jmatos1993@gmail.com>
2020-08-27 21:56:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -89,10 +89,6 @@ end
 
 
 
-
-
-
-
 def changing_alan
 	programmer_hash = 
  		{
@@ -111,7 +107,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-programmer_hash[:alan_kay][:known_for] = {"GUI"}
+programmer_hash[:alan_kay][:known_for] = {"alans_new_info" => "GUI"}
 programmer_hash
 end
 
